d342a1d2d57d664b2316543bd004b6f3b7132b7b
NizanthNiz <nizanthsugumaran@gmail.com>
2022-09-24 11:46:25 +0530

    cors_changes

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 var conn = require('./config/db');
 console.log("all is well")
 const app = express();
+const cors = require('cors');
 // app.use(app.router);
 // routes.initialize(app);
-const listener = app.listen(process.env.PORT || 12000, '0.0.0.0',() => {
+const listener = app.listen(process.env.PORT || 3004, '0.0.0.0',() => {
     console.log('Your app is listening on port ' + listener.address().port)
 })
 conn.connect((err)=>{
@@ -13,7 +14,7 @@ conn.connect((err)=>{
     else
     console.log('db connect')
 });
-// app.use(cors());
+app.use(cors());
 app.use(express.json()); 
 // app.use(bodyParser.urlencoded({ extended: false }))
 // app.use(bodyParser.json())
